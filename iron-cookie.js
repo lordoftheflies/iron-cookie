ed9127f5c07bee2ff8bf4d6b5628bbac298b2111
László Hegedűs <laszlo.hegedus@cherubits.hu>
2018-09-21 04:02:00 +0200

    Fix html

--- iron-cookie.js
+++ iron-cookie.js
@@ -15,22 +15,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-import { Element } from '@polymer/polymer/polymer-element.js';
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 
-const $_documentContainer = document.createElement('template');
-
-$_documentContainer.innerHTML = `<dom-module id="iron-cookie">
-    <template>
-        <style>
-            :host {
-                display: none;
-            }
-        </style>
-    </template>
-    
-</dom-module>`;
-
-document.head.appendChild($_documentContainer.content);
 /**
  * # `<iron-cookie>`
  *
